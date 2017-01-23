37d679d121cc22a82f391d7da979b2c3a1526e76
Fabien Thebaud <fthebaud@gmail.com>
2017-01-23 14:21:17 +0100

    1st draft

--- script-default-mode.js
+++ script-default-mode.js
@@ -1,26 +1,8 @@
 const defaultModeString = '************************* default mode (non strict) ********************';
 
 function logThis(obj) {
-  console.log('this = ' + obj + '\n\r');
-}
-
-const myFunctionExpression = function () {
-  console.log('In a function expression call');
-  logThis(this);
-};
-
-function myFunctionDeclaration() {
-  console.log('In a function expression call');
-  logThis(this);
-}
-
-function MyConstructor() {
-  console.log('In a constructor call (function + new)');
-  logThis(this);
-  this.method = function () {
-    console.log('In a method call');
-    logThis(this);
-  };
+  console.log(obj);
+  console.log('\n\r');
 }
 
 if (console.group) {
@@ -29,14 +11,87 @@ if (console.group) {
   console.log(defaultModeString);
 }
 
-// TESTS START HERE
 console.log('Outside of a function (global execution context)');
 logThis(this);
 
+console.log('In a function expression call');
+const myFunctionExpression = function () {
+  logThis(this);
+};
 myFunctionExpression();
+
+console.log('In a function expression call');
+
+function myFunctionDeclaration() {
+  logThis(this);
+}
 myFunctionDeclaration();
-const obj = new MyConstructor(); // eslint-disable-line no-unused-vars
+
+console.log('In a constructor call (function + new)');
+
+function MyObject() {
+  this.test = 'ok';
+  logThis(this);
+  this.method = function () {
+    logThis(this);
+  };
+  this.arrowMethod = () => logThis(this);
+}
+const obj = new MyObject(); // eslint-disable-line no-unused-vars
+
+console.log('In a method call');
 obj.method();
+
+console.log('Using call(thisArg) or apply(thisArg)');
+const fakeThis = {
+  fake: 'This'
+};
+myFunctionDeclaration.call(fakeThis);
+
+console.log('Arrow function in the global execution context');
+const arrowFunction = () => logThis(this);
+arrowFunction();
+
+console.log('Arrow function as a method');
+const objWithMethod = {
+  id: 'objWithMethod',
+  method1: () => logThis(this),
+  method2: function createMethod() {
+   return () => logThis(this);
+  }
+};
+objWithMethod.method1();
+objWithMethod.method2()();
+const fn = objWithMethod.method2;
+fn()();
+obj.arrowMethod();
+
+console.log('setTimeout');
+setTimeout(logThis, 0);
+setTimeout(function () {
+  logThis();
+}, 0);
+
+class Square {
+  constructor(side) {
+    this.side = side;
+    logThis(this);
+  }
+  method() {
+    logThis(this);
+  }
+  static methodStatic(){
+    logThis(this);
+  }
+}
+
+console.log('classes es6');
+console.log('constructor');
+const s = new Square(4);
+console.log('method');
+s.method();
+console.log('static method');
+Square.methodStatic();
 
 if (console.group) {
   console.groupEnd(defaultModeString);
--- script-strict-mode.js
+++ script-strict-mode.js
@@ -4,27 +4,17 @@
 
 
   function logThis2(obj) {
-    console.log('this = ' + obj + '\n\r');
+    console.log(obj);
+    console.log('\n\r');
   }
 
-  const myFunctionExpression2 = function () {
-    console.log('In a function expression call');
-    logThis2(this);
-  };
 
-  function myFunctionDeclaration2() {
-    console.log('In a function expression call');
-    logThis2(this);
-  }
 
-  function MyConstructor2() {
-    console.log('In a constructor call (function + new)');
-    logThis2(this);
-    this.method = function () {
-      console.log('In a method call');
-      logThis2(this);
-    };
-  }
+
+
+
+
+
 
   if (console.group) {
     console.group(strictModeString);
@@ -32,15 +22,87 @@
     console.log(strictModeString);
   }
 
-
-  // TESTS START HERE
   console.log('Outside of a function (global execution context)');
   logThis2(this);
 
+  console.log('In a function expression call');
+  const myFunctionExpression2 = function () {
+    logThis2(this);
+  };
   myFunctionExpression2();
+
+  console.log('In a function expression call');
+
+  function myFunctionDeclaration2() {
+    logThis2(this);
+  }
   myFunctionDeclaration2();
-  const objs = new MyConstructor2(); // eslint-disable-line no-unused-vars
-  objs.method();
+
+  console.log('In a constructor call (function + new)');
+
+  function MyObject2() {
+    this.test = 'ok';
+    logThis2(this);
+    this.method = function () {
+      logThis2(this);
+    };
+    this.arrowMethod = () => logThis2(this);
+  }
+  const obj2 = new MyObject2(); // eslint-disable-line no-unused-vars
+
+  console.log('In a method call');
+  obj2.method();
+
+  console.log('Using call(thisArg) or apply(thisArg)');
+  const fakeThis2 = {
+    fake: 'This'
+  };
+  myFunctionDeclaration2.call(fakeThis2);
+
+  console.log('Arrow function in the global execution context');
+  const arrowFunction2 = () => logThis2(this);
+  arrowFunction2();
+
+  console.log('Arrow function as a method');
+  const objWithMethod2 = {
+    id: 'objWithMethod',
+    method1: () => logThis2(this),
+    method2: function createMethod() {
+      return () => logThis2(this);
+    }
+  };
+  objWithMethod2.method1();
+  objWithMethod2.method2()();
+  const fn2 = objWithMethod2.method2;
+  fn2()();
+  obj2.arrowMethod();
+
+  console.log('setTimeout');
+  setTimeout(logThis2, 0);
+  setTimeout(function () {
+    logThis2();
+  }, 0);
+
+  class Square2 {
+  constructor(side) {
+    this.side = side;
+    logThis2(this);
+  }
+  method() {
+    logThis2(this);
+  }
+  static methodStatic(){
+    logThis2(this);
+  }
+}
+
+console.log('classes es6');
+console.log('constructor');
+const s2 = new Square2(5);
+console.log('method');
+s2.method();
+console.log('static method');
+Square2.methodStatic();
 
   if (console.group) {
     console.groupEnd(strictModeString);
